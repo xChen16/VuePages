8300278a0f0d059775110c5de888fba458061393
xChen16 <ares_0427@163.com>
2021-06-27 00:07:54 +0800

    init

---
+++ docs/.vuepress/config/nav/index.js
@@ -0,0 +1,12 @@
+module.exports = [
+  { text: '主页', link: '/', icon: 'reco-home' },
+  { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
+  {
+    text: 'Contact',
+    icon: 'reco-message',
+    items: [
+      { text: 'GitHub', link: 'https://github.com/xChen16', icon: 'reco-github' },
+
+    ]
+  }
+]
