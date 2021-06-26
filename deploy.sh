249b2d670bd4af9a0c6bca2858e8b41c414149a1
xChen16 <ares_0427@163.com>
2021-06-27 02:24:23 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ cd public
 git init
 git add -A
 git commit -m 'deploy'
-
+git pull --rebase origin  git@github.com:xChen16/VuePages.git ph-pages
 git push -f git@github.com:xChen16/VuePages.git ph-pages
 
 cd ../
